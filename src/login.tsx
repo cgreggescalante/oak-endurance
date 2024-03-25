5d309fce99c6e89297b437ade744198484ec8c1b
Conor <conor@johngregg.org>
2024-03-25 13:05:23 -0500

    OEA-25 Implement Login

--- src/login.tsx
+++ src/login.tsx
@@ -1,13 +1,57 @@
 import { Button, SafeAreaView, Text, TextInput } from "react-native";
 import AuthScreens from "@/styles/authScreens";
+import { useState } from "react";
+import { auth } from "../fireabaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export default ({ navigation }: { navigation: any }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+  const [attempting, setAttempting] = useState(false);
+
+  const handleLogin = () => {
+    setAttempting(true);
+
+    if (!verifyInput()) {
+      setAttempting(false);
+      return;
+    }
+
+    signInWithEmailAndPassword(auth, email, password)
+      .catch(() => setError("Invalid email or password"))
+      .finally(() => setAttempting(false));
+  };
+
+  const verifyInput = () => {
+    if (!email || !password) {
+      setError("All fields are required");
+      return false;
+    }
+
+    return true;
+  };
+
   return (
     <SafeAreaView style={AuthScreens.container}>
       <Text>Login</Text>
-      <TextInput placeholder="Email" />
-      <TextInput placeholder="Password" />
-      <Button title="Login" />
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType={"email-address"}
+        textContentType={"emailAddress"}
+        inputMode={"email"}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry={true}
+        textContentType={"password"}
+      />
+      <Button title="Login" disabled={attempting} onPress={handleLogin} />
       <Button
         title="Forgot Password"
         onPress={() => navigation.navigate("ForgotPassword")}
@@ -16,6 +60,7 @@ export default ({ navigation }: { navigation: any }) => {
         title="Create Account"
         onPress={() => navigation.navigate("Signup")}
       />
+      {error && <Text>{error}</Text>}
     </SafeAreaView>
   );
 };
